6bc970c6108f96fa22bd8448f61274b0bffef7a4
Mosbycr <46547100+Mosbycr@users.noreply.github.com>
2019-04-06 12:18:58 -0400

    Update login.js

--- login.js
+++ login.js
@@ -6,8 +6,7 @@
         callbacks: {
             signInSuccessWithAuthResult: function (authResult, redirectUrl) {
                 // User successfully signed in.
-                // Return type determines whether we continue the redirect automatically
-                // or whether we leave that to developer to handle.
+                
                 return true;
             },
             uiShown: function () {
@@ -20,13 +19,9 @@
         signInFlow: 'popup',
         signInSuccessUrl: 'main.html', //THIS IS WHERE YOU GO ONCE LOGGED IN
         signInOptions: [
-            // Leave the lines as is for the providers you want to offer your users.
-            // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-            // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            
             firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+            
         ],
         
     };
